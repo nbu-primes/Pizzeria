afb84851dad945b2d33d77bc4770196c34444590
nkolchakov <nikola.kolchakov@gmail.com>
2019-03-13 00:43:03 +0200

    add Recipe,Ingredient Dtos

---
+++ Pizzeria.Models/DTO/IngredientDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria.Models.DTO
+{
+    public class IngredientDto
+    {
+        public IngredientDto(string name, decimal price)
+        {
+            this.Name = name ?? throw new ArgumentNullException("name");
+            this.Price = price;
+        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Pizzeria.Models/DTO/RecipeDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pizzeria.Models.DTO
+{
+    public class RecipeDto
+    {
+        public RecipeDto(Recipe r)
+        {
+            this.Id = r.Id;
+            this.Description = r.Description;
+            this.ImageUrl = r.ImageUrl;
+            this.Price = r.Price;
+            this.Weight = r.Weight;
+            this.IsTemplate = r.IsTemplate;
+            this.Ingredients = r.RecipeIngredients
+                .Select(ri => new IngredientDto(ri.Ingredient.Name, ri.Ingredient.Price));
+        }
+
+        public Guid Id { get; set; }
+        
+        public string Description { get; set; }
+
+        public IEnumerable<IngredientDto> Ingredients { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public double Price { get; set; }
+
+        public double Weight { get; set; }
+
+        public bool IsTemplate { get; set; }
+    }
+}
